517d2c256d2ff41fc200f1a5e55283c7aa6fe2bf
Pietromuresu <muresupietro66@gmail.com>
2023-05-22 13:40:59 +0200

    Add new tasks

--- js/main.js
+++ js/main.js
@@ -1,15 +1,62 @@
 const {createApp} = Vue; 
 
 
+
 createApp ({
+
   data(){
     return{
-     
+    message: '',  
+    taskText : '',
+    tasks: [], 
     } 
   },
   methods: {
-   
+    getApi(){
+      console.log(this.tasks);
+      axios.get('server.php' )
+        .then(result => {
+          this.tasks = result.data
+          console.log(this.tasks);
+        })
+    },
 
-    
+    removeTask(){
+
+      
+    },
+
+    errorMessage(msg){
+      this.message = msg
+      setTimeout(() => {
+       this.message= ''
+      }, 2000)
+    },
+
+    newTask(){
+      
+        const newTask = {
+          
+            text : this.taskText,
+            done : false
+          
+        }
+        // const data = new FormData()
+        // data.append('text', this.taskText)
+        axios.post('server.php', newTask, {headers: {"Content-Type": "multipart/form-data"}})
+         .then(result => {
+
+          
+
+            this.tasks = result.data
+            console.log(this.tasks);
+          
+        })
+      console.log(this.taskText)
+       
+    }
+  },
+  mounted(){
+    this.getApi()
   }
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,22 +1,22 @@
 <?php
 
-$tasks = [
-        [
-          $text= "fare i compiti",
-          $done= false
-        ],
-        [
-          $text= "rinnovare l'assicurazione alla macchina",
-          $done= false
-        ],
-        [
-          $text= 'fare i compiti',
-          $done= false
-        ],
-        [
-          $text= 'fare i compiti',
-          $done= false
-        ],
-      ];
-      $message= '';
-      $taskText= '';
+$json_tasks = file_get_contents('tasks.json');
+
+$tasks = json_decode($json_tasks);
+
+
+
+if(isset($_POST['text']) && strlen($_POST['text']) > 1){
+  $newTask = [
+    "text" => $_POST['text'],
+    "done" => false
+  ];
+  
+  $tasks[] = $newTask;
+}
+
+file_put_contents('tasks.json', json_encode($tasks));
+
+
+header('Content-Type: application/json');
+echo json_encode($tasks);
